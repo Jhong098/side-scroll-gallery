b35b179f2a55999d1db14f63e37fdc681d5187b2
Jhong098 <jhong098@gmail.com>
2021-06-22 00:25:16 -0700

    initial setup

---
+++ js/app.js
@@ -0,0 +1,120 @@
+import * as THREE from "three";
+import fragment from "./shader/fragment.glsl";
+import vertex from "./shader/vertex.glsl";
+import * as dat from "dat.gui";
+
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+export default class Sketch {
+  constructor(options) {
+    this.scene = new THREE.Scene();
+    this.container = options.dom;
+
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
+
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(this.width, this.height);
+    this.renderer.setClearColor(0xeeeeee, 1);
+    this.renderer.physicallyCorrectLights = true;
+    this.renderer.outputEncoding = THREE.sRGBEncoding;
+
+    this.container.appendChild(this.renderer.domElement);
+
+    const frustumSize = 1;
+    const aspect = 1;
+    this.camera = new THREE.OrthographicCamera(
+      frustumSize / -2,
+      frustumSize / 2,
+      frustumSize / 2,
+      frustumSize / -2,
+      -1000,
+      1000
+    );
+    this.camera.position.set(0, 0, 2);
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+
+    this.isPlaying = true;
+    this.addObjects();
+    this.time = 0;
+    this.resize();
+    this.render();
+    this.setupResize();
+  }
+
+  settings() {
+    let that = this;
+    this.settings = {
+      progress: 0,
+    };
+    this.gui = new dat.GUI();
+    this.gui.add(this.settings, "progress", 0, 1, 0.01);
+  }
+
+  setupResize() {
+    window.addEventListener("resize", this.setupResize.bind(this));
+  }
+
+  resize() {
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
+    this.renderer.setSize(this.width, this.height);
+    this.camera.aspect = this.width / this.height;
+
+    this.imageAspect = 853 / 1280;
+    let a1;
+    let a2;
+    if (this.height / this.width > this.imageAspect) {
+      a1 = (this.width / this.height) * this.imageAspect;
+      a2 = 1;
+    } else {
+      a1 = 1;
+      a2 = this.height / this.width / this.imageAspect;
+    }
+
+    this.material.uniforms.resolution.value.x = this.width;
+    this.material.uniforms.resolution.value.y = this.height;
+    this.material.uniforms.resolution.value.z = a1;
+    this.material.uniforms.resolution.value.w = a2;
+
+    this.camera.updateProjectionMatrix();
+  }
+
+  addObjects() {
+    this.material = new THREE.ShaderMaterial({
+      fragmentShader: fragment,
+      vertexShader: vertex,
+      uniforms: {
+        progress: { type: "f", value: 0 },
+      },
+      side: THREE.DoubleSide,
+    });
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+    this.scene.add(this.mesh);
+  }
+
+  stop() {
+    this.isPlaying = false;
+  }
+
+  play() {
+    if (!this.isPlaying) {
+      this.render();
+      this.isPlaying = true;
+    }
+  }
+
+  render() {
+    if (!this.isPlaying) return;
+
+    this.time += 0.05;
+    this.material.uniforms.time.value = this.time;
+    window.requestAnimationFrame(this.render.bind(this));
+    this.renderer.render(this.scene, this.camera);
+  }
+}
+
+new Sketch({
+  dom: document.getElementById("container"),
+});
